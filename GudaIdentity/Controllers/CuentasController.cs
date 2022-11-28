a6e508337a65de0c5a48be8e875891ec7c7ac5f3
Nicolas Clementi <clementinicolas@gmail.com>
2022-11-28 15:03:48 -0300

    login

--- GudaIdentity/Controllers/CuentasController.cs
+++ GudaIdentity/Controllers/CuentasController.cs
@@ -55,27 +55,52 @@ namespace GudaIdentity.Controllers
                 if (resultado.Succeeded)
                 {
 
-                await signManager.SignInAsync(usuario, isPersistent: false);
+                    await signManager.SignInAsync(usuario, isPersistent: false);
 
-                return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
 
 
-                }               
+                }
                 ValidarErrores(resultado);
 
 
             }
             return View(rgViewModel);
-        } 
+        }
 
 
         // manejador de errores
-        private void ValidarErrores( IdentityResult resultado)
+        private void ValidarErrores(IdentityResult resultado)
         {
             foreach (var error in resultado.Errors)
             {
                 ModelState.AddModelError(String.Empty, error.Description);
             }
+        }
+
+        //Mostrar formulario de login
+        [HttpGet]
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Acceso (AccesoViewModel accViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var resultado = await signManager.PasswordSignInAsync(accViewModel.Email, accViewModel.Password, accViewModel.RemmemberMe,lockoutOnFailure: false);
+                if(resultado.Succeeded) 
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(String.Empty, "Acceso Invalido");
+                    return View(accViewModel);
+                }                     
+            }
+            return View(accViewModel);
         }
     }
 }
